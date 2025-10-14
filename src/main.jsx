8c8e1ff835bd3baacd5ff63631f3335ff95b43e4
DerandaBagas17 <111202314906@mhs.dinus.ac.id>
2025-10-14 10:37:48 +0700

    custom typography

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-
-// Hapus/komen baris index.css
-// import './index.css'; 
-// AKTIFKAN import ke App.css
 import './App.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
