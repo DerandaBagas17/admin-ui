9be1bcbe2ef2cf1ec3329e048e51b1a18ba25190
DerandaBagas17 <111202314906@mhs.dinus.ac.id>
2025-11-04 11:37:57 +0700

    UTS-Pemograman Sisi Klien

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -4,14 +4,14 @@ import { posts } from "./PostsData";
 
 function Exercise() {
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
+    <div className="min-h-screen bg-gray-100 p-25">
       <h1 className="text-3xl font-bold text-center text-special-red-2 mb-6">
         Post Cards
       </h1>
       <div className="grid 
                     sm:grid-cols-2 
                     md:grid-cols-3 
-                    lg:grid-cols-6   
+                    lg:grid-cols-5   
                     gap-4          
                     max-w-full      
                     mx-auto">
