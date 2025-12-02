95b687639202ab9f6e5e9a54a15babc73e98b87e
DerandaBagas17 <111202314906@mhs.dinus.ac.id>
2025-12-03 01:53:24 +0700

    react router dom

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AuthLayout from '../components/Layouts/AuthLayout'
 import FormSignIn from '../components/Fragments/FormSignIn'
+import { Link } from "react-router-dom";
 
 function signIn() {
   return (
--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AuthLayout from "../components/Layouts/AuthLayout";
 import FormSignUp from "../components/Fragments/FormSignUp";
+import { Link } from "react-router-dom";
 
 function signUp() {
   return (
